d66b589ba44a9c19db94d4d86006df3a06ffafdc
Max van Asten <maxvasten@gmail.com>
2026-01-19 20:18:18 +0100

    Added some more installs to install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,13 @@
-# RUN THIS SCRIPT TO ENSURE DEPENDENCIES
+sudo pacman -Syu
+# Install yay package manager
+sudo pacman -S --needed git base-devel
+mkdir ~/yay
+git clone https://aur.archlinux.org/yay-bin.git ~/yay
+cd ~/yay
+makepkg -si
+# Install nodejs and npm
+yay -S nodejs npm
+# Install bibletui
 sudo npm install -g bibletui
+# Install neovim
+yay -S neovim-nightly-bin

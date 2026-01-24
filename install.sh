96f00c21b67518f1e75fe107c15768ee7e91370c
Max van Asten <maxvasten@gmail.com>
2026-01-24 17:39:11 +0100

    Fix maybe

--- install.sh
+++ install.sh
@@ -11,13 +11,15 @@ rm -rf ~/yay
 # Install dependencies
 yay -S --noconfirm nodejs npm fastfetch neovim-nightly-bin
 # Install random_quote
+cd ~
 git clone https://github.com/maxvanasten/random_quote ~/random_quote
 cd ~/random_quote
-go build cmd/main.go
-sudo mv main /usr/bin/random_quote
+go build ~/random_quote/cmd/main.go
+sudo mv ~/random_quote/main /usr/bin/random_quote
 rm -rf ~/random_quote
 
 # Get dotfiles
+cd ~
 git clone https://github.com/maxvanasten/dotfiles ~/dotfiles
 cd ~/dotfiles
 bash sync.sh
